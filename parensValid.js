19cab0d158b8cdf4af170018fe5e3cf7c2c083b7
Kyle Stouffer <kylejstouffer@gmail.com>
2022-06-30 11:47:22 -0700

    parensValid Complete

---
+++ parensValid.js
@@ -0,0 +1,58 @@
+/* 
+Parens Valid
+Given an str that has parenthesis in it
+return whether the parenthesis are valid
+*/
+
+const str1 = "Y(3(p)p(3)r)s";
+const expected1 = true;
+
+const str2 = "N(0(p)3";
+const expected2 = false;
+// Explanation: not every parenthesis is closed.
+
+const str3 = "N(0)t ) 0(k";
+const expected3 = false;
+// Explanation: because the second ")" is premature: there is nothing open for it to close.
+
+const str4 = "a(b)()(c)";
+const expected4 = false;
+// Explanation: same number of opens and closes but the 2nd closing closes nothing.
+
+/**
+ * Determines whether the parenthesis in the given string are valid.
+ * Each opening parenthesis must have exactly one closing parenthesis.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str
+ * @returns {boolean} Whether the parenthesis are valid.
+ */
+function parensValid(str) {
+    let open = 0;
+    for (let i = 0; i < str.length; i++) {
+        const element = str[i];
+        if(element == "(")
+        {
+            open++;
+        }
+        if(element == ")")
+        {
+            open--;
+        }
+        if(open < 0)
+        {
+            return false
+        }
+    }
+    if(open == 0)
+    {
+        return true
+    }
+    return false
+
+}
+
+console.log(parensValid(str1))
+console.log(parensValid(str2))
+console.log(parensValid(str3))
+console.log(parensValid(str4))
